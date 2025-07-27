b5aa953fd81e28fd69fa5e06afac72a4817b1feb
rafay45 <rafayy579@gmail.com>
2025-07-27 23:47:31 +0500

    🔗🤩HusnaDeals

--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -5,10 +5,15 @@ export default function Home() {
   console.log("Products fetched:", products);
 
   return (
-    <section className="bg-gray-100 min-h-screen">
-      <div className="md:h-96 h-[550px] bg-pink-600 flex flex-wrap justify-around items-center">
+    <section
+      className="bg-gray-100 min-h-screen"
+    >
+      <div
+        className="md:h-96 h-[550px] bg-pink-600 flex flex-wrap justify-around items-center"
+      >
         <div
-          className="flex flex-col justify-centern items-center w-full md:w-[40%] border-2">
+          className="flex flex-col justify-centern items-center w-full md:w-[40%] border-2"
+        >
           <h1
             className="text-4xl font-bold"
           >Welcome to
@@ -20,12 +25,16 @@ export default function Home() {
             Affordable prices, trusted quality, and fast delivery to your doorstep.
             Shop smart, live better – only at HusnaDeals.</p>
         </div>
-        <div className="md:w-[40%] md:h-80 h-70 flex justify-center w-full">
+        <div
+          className="md:w-[40%] md:h-80 h-70 flex justify-center w-full"
+        >
           <img src={banner} alt="" />
         </div>
       </div>
 
-      <div className="grid grid-cols-1 mt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-7xl mx-auto">
+      <div
+        className="grid grid-cols-1 mt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-7xl mx-auto"
+      >
         {products.map((product) => (
           <div
             key={product.id}
@@ -38,15 +47,27 @@ export default function Home() {
                 className=" w-full h-full object-center rounded-md mb-4"
               />
             </div>
-            <h3 className="text-lg font-semibold text-gray-900 mb-1">
+            <h3
+              className="text-lg font-semibold text-gray-900 mb-1"
+            >
               {product.category}
             </h3>
-            <p className="text-gray-900 font-bold text-md mb-3">${product.price}</p>
-            <div className="flex items-center justify-between mt-4">
-              <span className="text-sm text-gray-500">Rating: {product.rating.rate}</span>
-              <span className="text-sm text-gray-500">({product.rating.count} reviews)</span>
+            <p
+              className="text-gray-900 font-bold text-md mb-3"
+            >${product.price}</p>
+            <div
+              className="flex items-center justify-between mt-4"
+            >
+              <span
+                className="text-sm text-gray-500"
+              >Rating: {product.rating.rate}</span>
+              <span
+                className="text-sm text-gray-500"
+              >({product.rating.count} reviews)</span>
             </div>
-            <button className="w-full bg-pink-600 text-white py-2 rounded-lg hover:bg-transparent hover:text-pink-600 hover:border-2 hover:py-1.5 cursor-pointer transition">
+            <button
+              className="w-full bg-pink-600 text-white py-2 rounded-lg hover:bg-transparent hover:text-pink-600 hover:border-2 hover:py-1.5 cursor-pointer transition"
+            >
               Add to Cart
             </button>
           </div>
