6777d5ff13b90ebcb9f2ac523f7c23801aa3a20c
rafay45 <rafayy579@gmail.com>
2025-07-28 14:30:06 +0500

    🔗🤩HusnaDeals

---
+++ src/components/container/switcher/switcher.js
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react";
+
+export const switcher = createContext({
+    theme: "light",
+    darktheme: () => { },
+    lightTheme: () => { }
+})
+
+export const switcherProvider = switcher.Provider
+
+export default function useTheme(){
+    return useContext(createContext)
+}
