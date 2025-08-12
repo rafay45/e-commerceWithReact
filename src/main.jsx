da775370d19302fad7acbafdaf6b215a23235374
rafay45 <rafayy579@gmail.com>
2025-08-12 14:59:19 +0500

    🔗🤩HusnaDeals

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,52 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
-import { Home, About, Contact, Cart } from './components/index.js'
 import App from './App.jsx'
+const Home = React.lazy(() => import('./components/screens/Home.jsx'));
+const About = React.lazy(() => import('./components/screens/About.jsx'));
+const Contact = React.lazy(() => import('./components/screens/Contact.jsx'));
+const Cart = React.lazy(() => import('./components/screens/Cart.jsx'));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />} >
-      <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<Contact />} />
-      <Route path='cart' element={<Cart />} />
+    <Route path="/" element={<App />}>
+      <Route
+        index
+        element={
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <Home />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path="about"
+        element={
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <About />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path="contact"
+        element={
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <Contact />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path="cart"
+        element={
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <Cart />
+          </React.Suspense>
+        }
+      />
     </Route>
-
   )
-)
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
