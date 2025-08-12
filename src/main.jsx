7984670c869245ae27e1a0c444c17efa85b973be
rafay45 <rafayy579@gmail.com>
2025-08-12 23:56:25 +0500

    🔗🤩HusnaDeals

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+// import { Home, About, Contact, Cart } from './components/index.js'
 import App from './App.jsx'
-const Home = React.lazy(() => import('./components/screens/Home.jsx'));
-const About = React.lazy(() => import('./components/screens/About.jsx'));
-const Contact = React.lazy(() => import('./components/screens/Contact.jsx'));
-const Cart = React.lazy(() => import('./components/screens/Cart.jsx'));
+const Home = React.lazy(() => import('./components/screens/Home.jsx'))
+const About = React.lazy(() => import('./components/screens/About.jsx'))
+const Contact = React.lazy(() => import('./components/screens/Contact.jsx'))
+const Cart = React.lazy(() => import('./components/screens/Cart.jsx'))
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
+    <Route path='/' element={<App />} >
       <Route
         index
         element={
@@ -21,32 +22,19 @@ const router = createBrowserRouter(
         }
       />
       <Route
-        path="about"
+        path='about'
         element={
           <React.Suspense fallback={<div>Loading...</div>}>
             <About />
           </React.Suspense>
-        }
+         }
       />
-      <Route
-        path="contact"
-        element={
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <Contact />
-          </React.Suspense>
-        }
-      />
-      <Route
-        path="cart"
-        element={
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <Cart />
-          </React.Suspense>
-        }
-      />
+      <Route path='contact' element={<Contact />} />
+      <Route path='cart' element={<Cart />} />
     </Route>
+
   )
-);
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
