42a511f0bae900891a0c15ef8eb51effc313bba7
Rafay <rafayy579@gmail.com>
2026-05-10 14:10:02 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Container, Navbar, Footer, CartProvider } from './components'
 import { Outlet } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
 
   return (
     <div>
       <CartProvider>
-      <Container>
-        <Navbar />
-        <Outlet />
-        <Footer />
-      </Container>
+        <ScrollToTop />
+        <Container>
+          <Navbar />
+          <Outlet />
+          <Footer />
+        </Container>
       </CartProvider>
     </div>
   )
